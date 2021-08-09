bde9abc8a9f721b82c6c4fed340cc33f5a765e6a
yadnyesh <yadnyeshrane1998@gmail.com>
2021-08-10 00:00:42 +0530

    PizaDeploy Strted

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import {BrowserRouter as Router,Switch,Route, Link} from 'react-router-dom';
+import Homes from './Pges/Homes';
+import About from './Pges/About'
+import  Nav from './componenets/Nav'
 
 function App() {
   return (
     <div className="App">
+       <>
+       <Router>
+              <Nav/>
+          <Switch>
+               <Route path="/" component={Homes} exact></Route>
+             <Route path="/about" component={About} exact></Route>
+
      
+         </Switch>
+       </Router>
+       </>
     </div>
   );
 }
---
+++ src/componenets/Nav.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+export default function Nav() {
+    return (
+        <>
+           <Link to="/">Home</Link>
+           <Link to="/about">About</Link>
+        </>
+
+    )
+}
